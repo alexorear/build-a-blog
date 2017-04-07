92383b1988a913c139bddc43e518203219b72791
Alex Orear <alex.orear@gmail.com>
2017-04-07 14:51:51 -0500

    Added error message if missing title or body text

--- main.py
+++ main.py
@@ -11,11 +11,26 @@ template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
+    def front_page(self, title = "", body = "", error = ""):
 
         t = jinja_env.get_template("front-page.html")
-        content = t.render()
+        content = t.render(title = title, body = body, error = error)
         self.response.write(content)
+
+    def get(self):
+
+        self.front_page()
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            self.response.write("Thanks!")
+        else:
+            error = "Please enter a title and some body text"
+            self.front_page(title, body, error)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
